8b102794e3536c4af9e397c641c25bd2bcc7c6d8
Pedro Brito <pp.n@hotmail.com>
2021-03-17 17:56:07 -0300

    subindo alteracoes

--- LMSApp/app/src/main/java/scarlet/machado/lmsapp/AcoesBtnActivity.kt
+++ LMSApp/app/src/main/java/scarlet/machado/lmsapp/AcoesBtnActivity.kt
@@ -1,14 +1,37 @@
 package scarlet.machado.lmsapp
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.toolbar.*
 
 class AcoesBtnActivity : DebugActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_configurar)
+        setContentView(R.layout.activity_acoesbtn)
 
-        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
 
     }
+
+override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    return true
+}
+
+override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    val id = item.itemId
+
+    if(id == android.R.id.home){
+        finish()
+    }
+
+    else if(id == android.R.id.home){
+        finish()
+    }
+
+    return super.onOptionsItemSelected(item)
+}
+
 }
--- LMSApp/app/src/main/java/scarlet/machado/lmsapp/TelaInicialActivity.kt
+++ LMSApp/app/src/main/java/scarlet/machado/lmsapp/TelaInicialActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.Button
 import android.widget.Toast
 import kotlinx.android.synthetic.main.toolbar.*
 
@@ -13,15 +15,46 @@ class TelaInicialActivity : DebugActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela_inicial)
 
-        var params = intent.extras
+ /*       var params = intent.extras
         val nome = params?.getString("nome")
         Toast.makeText(this, "Nome do usuário $nome", Toast.LENGTH_LONG).show()
-
+*/
         setSupportActionBar(toolbar)
+
+        val abrir_cardapio = "cardapio"
 
         supportActionBar?.title = "TELA PRINCIPAL"
 //        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+
+
+        val btn_cardapio = findViewById<Button>(R.id.abrir_cardapio)
+        val btn_comanda = findViewById<Button>(R.id.comandas)
+        val btn_pedido = findViewById<Button>(R.id.anotar_pedido)
+
+
+        btn_cardapio.setOnClickListener(View.OnClickListener {
+
+            val intent = Intent(this, AcoesBtnActivity::class.java) /*enviar para tela de cadastro*/
+            Toast.makeText(this, " NAO IMPLEMENTADO",
+                    Toast.LENGTH_LONG).show()
+            startActivity(intent)
+        })
+
+
+        btn_comanda.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, AcoesBtnActivity::class.java) /*enviar para tela de cadastro*/
+            Toast.makeText(this, " NAO IMPLEMENTADO",
+                    Toast.LENGTH_LONG).show()
+            startActivity(intent)
+        })
+
+        btn_pedido.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, AcoesBtnActivity::class.java) /*enviar para tela de cadastro*/
+            Toast.makeText(this, " NAO IMPLEMENTADO",
+                    Toast.LENGTH_LONG).show()
+            startActivity(intent)
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -33,6 +66,8 @@ class TelaInicialActivity : DebugActivity() {
         val id = item.itemId
 
         if(id == R.id.action_buscar){
+            Toast.makeText(this, "METODO DE BUSCAR NAO IMPLEMENTADO",
+                    Toast.LENGTH_LONG).show()
         }
         else if( id == R.id.action_add){
             Toast.makeText(this, "Botão de adicionar clicado",
@@ -42,11 +77,21 @@ class TelaInicialActivity : DebugActivity() {
 
         }
         else if (id == R.id.action_sair){
+            Toast.makeText(this, "Botão de sair clicado",
+                    Toast.LENGTH_LONG).show()
+            val intent = Intent(this, MainActivity::class.java) /*enviar para tela de cadastro*/
+            startActivity(intent)
 
         }
         else if (id == R.id.action_atualizar){
+            Toast.makeText(this, "METODO DE ATUALIZAR NAO IMPLEMENTADO",
+                    Toast.LENGTH_LONG).show()
 
         }else if(id == R.id.action_confg){
+            Toast.makeText(this, "Botão de sair clicado",
+                    Toast.LENGTH_LONG).show()
+            val intent = Intent(this, ConfigurarActivity::class.java) /*enviar para tela de cadastro*/
+            startActivity(intent)
 
         }else if(id == android.R.id.home){
             finish()
